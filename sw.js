c35250b215c2004e407a1ef55e0aa29b9f5ced87
cozestudiotextil-star <cozestudiotextil@gmail.com>
2026-07-06 15:03:10 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const C='coz-v1';
+const C='coz-v2';
 const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('fetch',e=>{ const req=e.request;
+  if(req.mode==='navigate'){ e.respondWith(fetch(req).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put('./index.html',cp));return r;}).catch(()=>caches.match('./index.html'))); return; }
+  e.respondWith(caches.match(req).then(r=>r||fetch(req))); });
